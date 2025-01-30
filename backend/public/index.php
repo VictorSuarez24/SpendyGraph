4a9bb12d92088d2942ad88732a152a72f337a92a
Victor Suárez <vitillosuarezcruz@gmail.com>
2025-01-30 16:02:18 +0000

    Credentials (cookies)
    Allow Cookies

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,6 +5,9 @@ header("Access-Control-Allow-Origin: *")
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+//Cookies 
+header("Access-Control-Allow-Credentials: true"); 
+
 require_once '../core/Router.php';
 
 //Obtener la URL de la petición
